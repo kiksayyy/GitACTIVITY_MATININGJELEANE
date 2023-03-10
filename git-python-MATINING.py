a600994ca10a857f2a334f38921e1c847f3d5631
MatiningJeleane <matiningjeleane@gmail.com>
2023-03-10 11:32:02 +0800

    MyeducationalBackground

--- git-python-MATINING.py
+++ git-python-MATINING.py
@@ -8,3 +8,7 @@ Created on Fri Mar 10 10:10:02 2023
 print("FULL NAME: JELEANE M. MATINING")
 print("ADDRESS: PILI BOAC MARINDUQUE")
 print("BIRTHDAY: 09/25/2002")
+
+print("College: Marinduque State College")
+print("Course: Bachelor of Science Information Technology")
+print("Year and Section: 2nd year and Section B")
