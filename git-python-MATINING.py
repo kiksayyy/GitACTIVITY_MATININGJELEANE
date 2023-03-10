e0abca8bf313294106421a7b8fc5e5aaf85a4e68
MatiningJeleane <matiningjeleane@gmail.com>
2023-03-10 11:25:41 +0800

    My Personal Info

---
+++ git-python-MATINING.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 10:10:02 2023
+
+@author: Shiela Marie
+"""
+
+print("FULL NAME: JELEANE M. MATINING")
+print("ADDRESS: PILI BOAC MARINDUQUE")
+print("BIRTHDAY: 09/25/2002")
